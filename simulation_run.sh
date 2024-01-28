92bb5e1e2bef251b9e35fcfaa14caab925466b1c
Nowell Closser <nowell91@gmail.com>
2024-01-27 22:46:54 -0500

    Add python module loading, package installation, and correct personalized dirs to simulation script

--- simulation_run.sh
+++ simulation_run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-set -e
-set -x
-
 #SBATCH -n 4                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 0-25:00          # Runtime in D-HH:MM, minimum of 10 minutes
@@ -10,13 +7,23 @@ set -x
 #SBATCH -o jobs_out/may3/T=50_n=100_actionC=1_N=2k_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e jobs_out/may3/T=50_n=100_actionC=1_N=2k_%j.err  # File to which STDERR will be written, %j inserts jobid
 
-# module load Anaconda3/2020.11
-# cd /n/home02/kellywzhang
+# Stop on nonzero exit codes and use of undefined variables
+set -eu
 
-# source activate py39
-# which python
+# Load Python 3.10, among other things
+module load Mambaforge/22.11.1-fasrc01
 
-# cd /n/home02/kellywzhang/adaptive-sandwich
+cd ~
+
+# Make virtualenv if necessary, and then activate it
+if test -d venv; then
+  python3 -m venv venv
+fi
+source venv/bin/activate
+
+# Now install all requirements.  This is incremental, so it's ok to do every time.
+cd ~/adaptive-sandwich
+pip install --no-index -r requirements.txt
 
 # T=25,50
 # Steepness=0.5,1,2?
@@ -50,8 +57,10 @@ n=100
 #n = 1000, 100, 500
 #steepness = 1 2 0.5
 
-#TODO: use this
-save_dir=/n/murphy_lab/lab/kellywzhang/inference_after_pooling/results/may3
+#TODO: use this in after study analysis
+now = $(printf "%(%F_%H%M%S)T")
+save_dir="/n/murphy_lab/lab/nclosser/adaptive_sandwich_simulation_results/${now}"
+mkdir -p $save_dir
 
 #for steepness in 1 2 0.5 5
 for steepness in 1.0 0.5 5.0
@@ -60,14 +69,14 @@ do
     for synthetic_mode in 'delayed_1_dosage' 'delayed_5_dosage' #'delayed_2_dosage'
     do
         # Simulate an RL study with the supplied arguments.  (We do just one repetition)
-        python rl_study_simulation.py --T=$T --N=$N --n=$n --min_users=$min_users --decisions_between_updates $decisions_between_updates --recruit_n $recruit_n --recruit_t $recruit_t --synthetic_mode $synthetic_mode --steepness $steepness --RL_alg $RL_alg --err_corr $err_corr --alg_state_feats $alg_state_feats --action_centering $action_centering
+        python rl_study_simulation.py --T=$T --N=$N --n=$n --min_users=$min_users --decisions_between_updates $decisions_between_updates --recruit_n $recruit_n --recruit_t $recruit_t --synthetic_mode $synthetic_mode --steepness $steepness --RL_alg $RL_alg --err_corr $err_corr --alg_state_feats $alg_state_feats --action_centering $action_centering --save_dir=$save_dir
 
         # Create a convenience variable that holds the output folder for the last script
-        output_folder="simulated_data/synthetic_mode=${synthetic_mode}_alg=${RL_alg}_T=${T}_n=${n}_recruitN=${recruit_n}_decisionsBtwnUpdates=${decisions_between_updates}_steepness=${steepness}_algfeats=${alg_state_feats}_errcorr=${err_corr}_actionC=${action_centering}"
+        output_folder="${save_dir}/simulated_data/synthetic_mode=${synthetic_mode}_alg=${RL_alg}_T=${T}_n=${n}_recruitN=${recruit_n}_decisionsBtwnUpdates=${decisions_between_updates}_steepness=${steepness}_algfeats=${alg_state_feats}_errcorr=${err_corr}_actionC=${action_centering}"
 
         # Loop through each dataset created in the simulation (determined by number of Monte carlo repetitions)
         # and do after-study analysis
-        python after_study_analysis.py analyze-multiple-datasets-and-compare-to-empirical-variance --input_folder="${output_folder}" --study_dataframe_pickle_filename="study_df.pkl" --rl_algorithm_object_pickle_filename="study_RLalg.pkl"
+        python after_study_analysis.py analyze-multiple-datasets-and-compare-to-empirical-variance --input_folder="${output_folder}" --study_dataframe_pickle_filename="study_df.pkl" --rl_algorithm_object_pickle_filename="study_RLalg.pkl" --save_dir=$save_dir
     done
 
 done

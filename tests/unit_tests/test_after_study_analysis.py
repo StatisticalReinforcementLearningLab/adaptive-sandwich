6c680d532fd3bdecd87ca04debb5a176825cf864
Nowell Closser <nowell91@gmail.com>
2024-09-18 02:29:29 -0400

    Add testing docs and placeholder test

--- tests/unit_tests/test_after_study_analysis.py
+++ tests/unit_tests/test_after_study_analysis.py
@@ -1140,6 +1140,7 @@ def test_calculate_upper_left_bread_inverse_2_decs_btwn_updates_action_probs_in_
         ),
     )
 
+
 @pytest.mark.skip(reason="Need to add")
 def test_calculate_upper_left_bread_inverse_incremental_recruitment(self):
     raise NotImplementedError()
@@ -1150,6 +1151,19 @@ def test_calculate_upper_left_bread_agnostic_to_pi_times_that_start_before_first
     self,
 ):
     # This was a bug.  We were automatically assuming the algo statistics dict
+    # started at the first time after the first update, which used to be true
+    # before we generalized things.  Now it typically starts at time 1, grabbing
+    # pi and weight gradients from the beginning.  We don't want to assume
+    # either way. Make sure both approaches work.
+    raise NotImplementedError()
+
+
+@pytest.mark.skip(reason="Need to add")
+def test_form_bread_inverse_agnostic_to_pi_times_that_start_before_first_update(
+    self,
+):
+    # As above for the RL side, this was a bug for the inference side.  We were
+    # automatically assuming the algo statistics dict
     # started at the first time after the first update, which used to be true
     # before we generalized things.  Now it typically starts at time 1, grabbing
     # pi and weight gradients from the beginning.  We don't want to assume

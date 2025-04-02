1a3c8acd2964ab2823d3aca46928f2f5ac8b07b6
Nowell Closser <nowell91@gmail.com>
2025-04-02 03:37:59 -0400

    ungitignore parallel files, since deploy script ignores gitignored files

---
+++ run_and_analysis_parallel_synthetic.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+#SBATCH -n 4                                                                                                # Number of cores
+#SBATCH -N 1                                                                                                # Ensure that all cores are on one machine
+#SBATCH -t 0-0:20                                                                                           # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH --mem=5G                                                                                            # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -p serial_requeue                                                                                   # Target Partition
+#SBATCH -o /n/netscratch/murphy_lab/Lab/nclosser/adaptive_sandwich_simulation_results/%A/slurm.%a.out       # STDOUT
+#SBATCH -e /n/netscratch/murphy_lab/Lab/nclosser/adaptive_sandwich_simulation_results/%A/slurm.%a.out       # STDERR
+#SBATCH --mail-type=END                                                                                     # This command would send an email when the job ends.
+#SBATCH --mail-type=FAIL                                                                                    # This command would send an email when the job ends.
+#SBATCH --mail-user=nowellclosser@g.harvard.edu                                                             # Email to which notifications will be sent
+
+# Note this script is to be run with something like the following command:
+# sbatch --array=[0-99] run_and_analysis_parallel_synthetic.sh --T=25 --n=100 --recruit_n=100 --recruit_t=1
+
+# To analyze, run simulation_collect_analyses.sh as described in the
+# output of one of the simulation runs.
+
+# If running on GPU, the following can be used:
+# S BATCH -p gpu_requeue                                                       # Target Partition
+# S BATCH --gres=gpu:1                                                         # Request a GPU
+
+# Stop on nonzero exit codes and use of undefined variables, and print all commands
+set -eu
+
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Parsing options.
+
+die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
+needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
+
+# Arguments that affect RL study simulation side and then inference through
+# that.
+T=10
+decisions_between_updates=1
+recruit_t=1
+n=100
+recruit_n=$n
+synthetic_mode='delayed_1_dosage'
+steepness=0.0
+RL_alg="sigmoid_LS"
+err_corr='time_corr'
+alg_state_feats="intercept,past_reward"
+action_centering_RL=0
+dynamic_seeds=0
+
+# Arguments that only affect inference side.
+in_study_col_name="in_study"
+action_col_name="action"
+policy_num_col_name="policy_num"
+calendar_t_col_name="calendar_t"
+user_id_col_name="user_id"
+action_prob_col_name="action1prob"
+action_prob_func_filename="functions_to_pass_to_analysis/get_action_1_prob_pure.py"
+action_prob_func_args_beta_index=0
+alg_update_func_filename="functions_to_pass_to_analysis/get_least_squares_loss_rl.py"
+alg_update_func_type="loss"
+alg_update_func_args_beta_index=0
+alg_update_func_args_action_prob_index=5
+alg_update_func_args_action_prob_times_index=6
+inference_func_filename="functions_to_pass_to_analysis/get_least_squares_loss_inference_action_centering.py"
+inference_func_args_theta_index=0
+inference_func_type="loss"
+theta_calculation_func_filename="functions_to_pass_to_analysis/estimate_theta_least_squares_action_centering.py"
+suppress_interactive_data_checks=1
+suppress_all_data_checks=0
+small_sample_correction="none"
+
+# Parse single-char options as directly supported by getopts, but allow long-form
+# under - option.  The :'s signify that arguments are required for these options.
+# Note that the N argument is not supplied here: the number of simulations is
+# determined by the number of jobs in the slurm job array.
+while getopts T:t:n:u:d:r:e:f:a:s:y:i:c:p:C:U:P:b:l:Z:B:D:j:E:I:h:g:H:F:Q:q:z:-: OPT; do
+  # support long options: https://stackoverflow.com/a/28466267/519360
+  if [ "$OPT" = "-" ]; then   # long option: reformulate OPT and OPTARG
+    OPT="${OPTARG%%=*}"       # extract long option name
+    OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
+    OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+  fi
+  case "$OPT" in
+    T  | max_time )                                     needs_arg; T="$OPTARG" ;;
+    t  | recruit_t )                                    needs_arg; recruit_t="$OPTARG" ;;
+    n  | num_users )                                    needs_arg; n="$OPTARG" ;;
+    u  | recruit_n )                                    needs_arg; recruit_n="$OPTARG" ;;
+    d  | decisions_between_updates )                    needs_arg; decisions_between_updates="$OPTARG" ;;
+    r  | RL_alg )                                       needs_arg; RL_alg="$OPTARG" ;;
+    e  | err_corr )                                     needs_arg; err_corr="$OPTARG" ;;
+    f  | alg_state_feats )                              needs_arg; alg_state_feats="$OPTARG" ;;
+    a  | action_centering_RL )                          needs_arg; action_centering_RL="$OPTARG" ;;
+    s  | steepness )                                    needs_arg; steepness="$OPTARG" ;;
+    y  | synthetic_mode )                               needs_arg; synthetic_mode="$OPTARG" ;;
+    i  | in_study_col_name )                            needs_arg; in_study_col_name="$OPTARG" ;;
+    c  | action_col_name )                              needs_arg; action_col_name="$OPTARG" ;;
+    p  | policy_num_col_name )                          needs_arg; policy_num_col_name="$OPTARG" ;;
+    C  | calendar_t_col_name )                          needs_arg; calendar_t_col_name="$OPTARG" ;;
+    U  | user_id_col_name )                             needs_arg; user_id_col_name="$OPTARG" ;;
+    E  | action_prob_col_name )                         needs_arg; action_prob_col_name="$OPTARG" ;;
+    P  | action_prob_func_filename )                    needs_arg; action_prob_func_filename="$OPTARG" ;;
+    b  | action_prob_func_args_beta_index )             needs_arg; action_prob_func_args_beta_index="$OPTARG" ;;
+    l  | alg_update_func_filename )                     needs_arg; alg_update_func_filename="$OPTARG" ;;
+    Z  | alg_update_func_type )                         needs_arg; alg_update_func_type="$OPTARG" ;;
+    B  | alg_update_func_args_beta_index )              needs_arg; alg_update_func_args_beta_index="$OPTARG" ;;
+    D  | alg_update_func_args_action_prob_index )       needs_arg; alg_update_func_args_action_prob_index="$OPTARG" ;;
+    j  | alg_update_func_args_action_prob_times_index ) needs_arg; alg_update_func_args_action_prob_times_index="$OPTARG" ;;
+    I  | inference_func_filename )                      needs_arg; inference_func_filename="$OPTARG" ;;
+    h  | inference_func_args_theta_index )              needs_arg; inference_func_args_theta_index="$OPTARG" ;;
+    g  | inference_func_type )                          needs_arg; inference_func_type="$OPTARG" ;;
+    H  | theta_calculation_func_filename )              needs_arg; theta_calculation_func_filename="$OPTARG" ;;
+    F  | dynamic_seeds )                                needs_arg; dynamic_seeds="$OPTARG" ;;
+    Q  | suppress_interactive_data_checks )             needs_arg; suppress_interactive_data_checks="$OPTARG" ;;
+    q  | suppress_all_data_checks )                     needs_arg; suppress_all_data_checks="$OPTARG" ;;
+    z  | small_sample_correction )                      needs_arg; small_sample_correction="$OPTARG" ;;
+    \? )                                        exit 2 ;;  # bad short option (error reported via getopts)
+    * )                                         die "Illegal option --$OPT" ;; # bad long option
+  esac
+done
+shift $((OPTIND-1)) # remove parsed options and args from $@ list
+
+# Load Python 3.10, among other things
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Loading mamba and CUDA modules.
+module load Mambaforge/22.11.1-fasrc01
+module load cuda/12.2.0-fasrc01
+
+# Make virtualenv if necessary, and then activate it
+cd ~
+if ! test -d venv; then
+  echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Creating venv, as it did not exist.
+  python3 -m venv venv
+fi
+source venv/bin/activate
+
+# Now install all Python requirements.  This is incremental, so it's ok to do every time.
+cd ~/adaptive-sandwich
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Making sure Python requirements are installed.
+pip install -r cluster_simulation_requirements.txt
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: All Python requirements installed.
+
+save_dir_prefix="/n/netscratch/murphy_lab/Lab/nclosser/adaptive_sandwich_simulation_results/${SLURM_ARRAY_JOB_ID}"
+
+if test -d save_dir_prefix; then
+  die 'Output directory already exists. Please supply a unique label, perhaps a datetime.'
+fi
+save_dir="${save_dir_prefix}/${SLURM_ARRAY_TASK_ID}"
+save_dir_glob="${save_dir_prefix}/*"
+mkdir -p "$save_dir"
+
+# Simulate an RL study with the supplied arguments.  (We do just one repetition)
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Beginning RL simulations.
+python rl_study_simulation.py \
+  --T=$T \
+  --N=1 \
+  --parallel_task_index=$SLURM_ARRAY_TASK_ID \
+  --n=$n \
+  --decisions_between_updates=$decisions_between_updates \
+  --recruit_n=$recruit_n \
+  --recruit_t=$recruit_t \
+  --synthetic_mode=$synthetic_mode \
+  --steepness=$steepness \
+  --RL_alg=$RL_alg \
+  --err_corr=$err_corr \
+  --alg_state_feats=$alg_state_feats \
+  --action_centering=$action_centering_RL \
+  --save_dir=$save_dir \
+  --dynamic_seeds=$dynamic_seeds
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Finished RL simulations.
+
+# Create a convenience variable that holds the output folder for the last script
+save_dir_suffix="simulated_data/synthetic_mode=${synthetic_mode}_alg=${RL_alg}_T=${T}_n=${n}_recruitN=${recruit_n}_decisionsBtwnUpdates=${decisions_between_updates}_steepness=${steepness}_algfeats=${alg_state_feats}_errcorr=${err_corr}_actionC=${action_centering_RL}"
+output_folder="${save_dir}/${save_dir_suffix}"
+output_folder_glob="${save_dir_glob}/${save_dir_suffix}"
+
+# Analyze dataset created in the above simulation
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Beginning after-study analysis.
+python after_study_analysis.py analyze-dataset \
+  --study_df_pickle="${output_folder}/exp=1/study_df.pkl" \
+  --action_prob_func_filename=$action_prob_func_filename \
+  --action_prob_func_args_pickle="${output_folder}/exp=1/pi_args.pkl" \
+  --action_prob_func_args_beta_index=$action_prob_func_args_beta_index \
+  --alg_update_func_filename=$alg_update_func_filename \
+  --alg_update_func_type=$alg_update_func_type \
+  --alg_update_func_args_pickle="${output_folder}/exp=1/rl_update_args.pkl" \
+  --alg_update_func_args_beta_index=$alg_update_func_args_beta_index \
+  --alg_update_func_args_action_prob_index=$alg_update_func_args_action_prob_index \
+  --alg_update_func_args_action_prob_times_index=$alg_update_func_args_action_prob_times_index \
+  --inference_func_filename=$inference_func_filename \
+  --inference_func_args_theta_index=$inference_func_args_theta_index \
+  --inference_func_type=$inference_func_type \
+  --theta_calculation_func_filename=$theta_calculation_func_filename \
+  --in_study_col_name=$in_study_col_name \
+  --action_col_name=$action_col_name \
+  --policy_num_col_name=$policy_num_col_name \
+  --calendar_t_col_name=$calendar_t_col_name \
+  --user_id_col_name=$user_id_col_name \
+  --action_prob_col_name=$action_prob_col_name \
+  --suppress_interactive_data_checks=$suppress_interactive_data_checks \
+  --suppress_all_data_checks=$suppress_all_data_checks \
+  --small_sample_correction=$small_sample_correction
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Finished after-study analysis.
+
+echo $(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: Simulation complete.
+echo "$(date +"%Y-%m-%d %T") run_and_analysis_parallel_synthetic.sh: When all jobs have completed, you may collect and summarize the analyses with: bash simulation_collect_analyses.sh --input_glob=${output_folder_glob}/exp=1/analysis.pkl"

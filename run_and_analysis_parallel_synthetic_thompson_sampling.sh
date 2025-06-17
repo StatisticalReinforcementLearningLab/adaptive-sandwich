6a537414883f5a23207993391ca917e830c23a2d
Nowell Closser <nowell91@gmail.com>
2025-06-17 14:38:08 -0400

    Add update cadence offset argument

--- run_and_analysis_parallel_synthetic_thompson_sampling.sh
+++ run_and_analysis_parallel_synthetic_thompson_sampling.sh
@@ -31,6 +31,7 @@ needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
 # Arguments that affect RL study simulation side
 T=140
 decisions_between_updates=14
+update_cadence_offset=0
 min_update_time=14
 recruit_t=2 # How many UPDATES between recruitments
 n=100
@@ -76,7 +77,7 @@ small_sample_correction="none"
 # under - option.  The :'s signify that arguments are required for these options.
 # Note that the N argument is not supplied here: the number of simulations is
 # determined by the number of jobs in the slurm job array.
-while getopts T:t:n:u:d:r:e:f:a:y:Y:i:c:p:C:U:P:b:l:Z:B:D:j:E:I:h:g:H:F:Q:q:z:-: OPT; do
+while getopts T:t:n:u:d:o:r:e:f:a:y:Y:i:c:p:C:U:P:b:l:Z:B:D:j:E:I:h:g:H:F:Q:q:z:-: OPT; do
   # support long options: https://stackoverflow.com/a/28466267/519360
   if [ "$OPT" = "-" ]; then   # long option: reformulate OPT and OPTARG
     OPT="${OPTARG%%=*}"       # extract long option name
@@ -89,6 +90,7 @@ while getopts T:t:n:u:d:r:e:f:a:y:Y:i:c:p:C:U:P:b:l:Z:B:D:j:E:I:h:g:H:F:Q:q:z:-:
     n  | num_users )                                    needs_arg; n="$OPTARG" ;;
     u  | recruit_n )                                    needs_arg; recruit_n="$OPTARG" ;;
     d  | decisions_between_updates )                    needs_arg; decisions_between_updates="$OPTARG" ;;
+    o  | update_cadence_offset )                        needs_arg; update_cadence_offset="$OPTARG" ;;
     r  | RL_alg )                                       needs_arg; RL_alg="$OPTARG" ;;
     e  | err_corr )                                     needs_arg; err_corr="$OPTARG" ;;
     f  | alg_state_feats )                              needs_arg; alg_state_feats="$OPTARG" ;;
@@ -169,6 +171,7 @@ python rl_study_simulation.py \
   --parallel_task_index=$SLURM_ARRAY_TASK_ID \
   --n=$n \
   --decisions_between_updates=$decisions_between_updates \
+  --update_cadence_offset=$update_cadence_offset \
   --recruit_n=$recruit_n \
   --recruit_t=$recruit_t \
   --synthetic_mode=$synthetic_mode \

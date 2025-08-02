4e4eb516e143d6d7bda2bcc9a2afcceee7d71d23
Nowell Closser <nowell91@gmail.com>
2025-08-02 15:24:16 -0400

    update unit tests after small-sample-correction facilitating restructuring to expose individual meat contributions

--- tests/unit_tests/test_after_study_analysis.py
+++ tests/unit_tests/test_after_study_analysis.py
@@ -347,31 +347,26 @@ def test_construct_single_user_weighted_estimating_function_stacker_simplest(
     )
     np.testing.assert_allclose(
         result[0],
-        jnp.mean(
-            jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]), axis=0
-        ),
+        jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]),
         rtol=1e-6,
     )
     np.testing.assert_array_equal(
         result[1][0],
-        result[0],
+        jnp.mean(result[0], axis=0),
     )
     np.testing.assert_allclose(
         result[1][1],
-        jnp.mean(
-            jnp.array(
-                [
-                    jnp.outer(
-                        expected_weighted_stack_1,
-                        expected_weighted_stack_1,
-                    ),
-                    jnp.outer(
-                        expected_weighted_stack_2,
-                        expected_weighted_stack_2,
-                    ),
-                ]
-            ),
-            axis=0,
+        jnp.array(
+            [
+                jnp.outer(
+                    expected_weighted_stack_1,
+                    expected_weighted_stack_1,
+                ),
+                jnp.outer(
+                    expected_weighted_stack_2,
+                    expected_weighted_stack_2,
+                ),
+            ]
         ),
         rtol=1e-6,
     )
@@ -689,31 +684,26 @@ def test_construct_single_user_weighted_estimating_function_stacker_estimating_f
     )
     np.testing.assert_allclose(
         result[0],
-        jnp.mean(
-            jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]), axis=0
-        ),
+        jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]),
         rtol=1e-6,
     )
     np.testing.assert_array_equal(
         result[1][0],
-        result[0],
+        jnp.mean(result[0], axis=0),
     )
     np.testing.assert_allclose(
         result[1][1],
-        jnp.mean(
-            jnp.array(
-                [
-                    jnp.outer(
-                        expected_weighted_stack_1,
-                        expected_weighted_stack_1,
-                    ),
-                    jnp.outer(
-                        expected_weighted_stack_2,
-                        expected_weighted_stack_2,
-                    ),
-                ]
-            ),
-            axis=0,
+        jnp.array(
+            [
+                jnp.outer(
+                    expected_weighted_stack_1,
+                    expected_weighted_stack_1,
+                ),
+                jnp.outer(
+                    expected_weighted_stack_2,
+                    expected_weighted_stack_2,
+                ),
+            ]
         ),
         rtol=1e-6,
     )
@@ -1291,31 +1281,26 @@ def test_construct_single_user_weighted_estimating_function_stacker_different_be
 
     np.testing.assert_allclose(
         result[0],
-        jnp.mean(
-            jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]), axis=0
-        ),
+        jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]),
         rtol=1e-6,
     )
     np.testing.assert_array_equal(
         result[1][0],
-        result[0],
+        jnp.mean(result[0], axis=0),
     )
     np.testing.assert_allclose(
         result[1][1],
-        jnp.mean(
-            jnp.array(
-                [
-                    jnp.outer(
-                        expected_weighted_stack_1,
-                        expected_weighted_stack_1,
-                    ),
-                    jnp.outer(
-                        expected_weighted_stack_2,
-                        expected_weighted_stack_2,
-                    ),
-                ]
-            ),
-            axis=0,
+        jnp.array(
+            [
+                jnp.outer(
+                    expected_weighted_stack_1,
+                    expected_weighted_stack_1,
+                ),
+                jnp.outer(
+                    expected_weighted_stack_2,
+                    expected_weighted_stack_2,
+                ),
+            ]
         ),
         rtol=1e-6,
     )
@@ -1837,31 +1822,26 @@ def test_construct_single_user_weighted_estimating_function_stacker_incremental_
 
     np.testing.assert_allclose(
         result[0],
-        jnp.mean(
-            jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]), axis=0
-        ),
+        jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]),
         rtol=1e-6,
     )
     np.testing.assert_array_equal(
         result[1][0],
-        result[0],
+        jnp.mean(result[0], axis=0),
     )
     np.testing.assert_allclose(
         result[1][1],
-        jnp.mean(
-            jnp.array(
-                [
-                    jnp.outer(
-                        expected_weighted_stack_1,
-                        expected_weighted_stack_1,
-                    ),
-                    jnp.outer(
-                        expected_weighted_stack_2,
-                        expected_weighted_stack_2,
-                    ),
-                ]
-            ),
-            axis=0,
+        jnp.array(
+            [
+                jnp.outer(
+                    expected_weighted_stack_1,
+                    expected_weighted_stack_1,
+                ),
+                jnp.outer(
+                    expected_weighted_stack_2,
+                    expected_weighted_stack_2,
+                ),
+            ]
         ),
         rtol=1e-6,
     )
@@ -2286,31 +2266,26 @@ def test_construct_single_user_weighted_estimating_function_stacker_multiple_dec
 
     np.testing.assert_allclose(
         result[0],
-        jnp.mean(
-            jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]), axis=0
-        ),
+        jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]),
         rtol=1e-6,
     )
     np.testing.assert_array_equal(
         result[1][0],
-        result[0],
+        jnp.mean(result[0], axis=0),
     )
     np.testing.assert_allclose(
         result[1][1],
-        jnp.mean(
-            jnp.array(
-                [
-                    jnp.outer(
-                        expected_weighted_stack_1,
-                        expected_weighted_stack_1,
-                    ),
-                    jnp.outer(
-                        expected_weighted_stack_2,
-                        expected_weighted_stack_2,
-                    ),
-                ]
-            ),
-            axis=0,
+        jnp.array(
+            [
+                jnp.outer(
+                    expected_weighted_stack_1,
+                    expected_weighted_stack_1,
+                ),
+                jnp.outer(
+                    expected_weighted_stack_2,
+                    expected_weighted_stack_2,
+                ),
+            ]
         ),
         rtol=1e-6,
     )
@@ -3017,31 +2992,26 @@ def test_construct_single_user_weighted_estimating_function_stacker_use_action_p
 
     np.testing.assert_allclose(
         result[0],
-        jnp.mean(
-            jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]), axis=0
-        ),
+        jnp.array([expected_weighted_stack_1, expected_weighted_stack_2]),
         rtol=1e-6,
     )
     np.testing.assert_array_equal(
         result[1][0],
-        result[0],
+        jnp.mean(result[0], axis=0),
     )
     np.testing.assert_allclose(
         result[1][1],
-        jnp.mean(
-            jnp.array(
-                [
-                    jnp.outer(
-                        expected_weighted_stack_1,
-                        expected_weighted_stack_1,
-                    ),
-                    jnp.outer(
-                        expected_weighted_stack_2,
-                        expected_weighted_stack_2,
-                    ),
-                ]
-            ),
-            axis=0,
+        jnp.array(
+            [
+                jnp.outer(
+                    expected_weighted_stack_1,
+                    expected_weighted_stack_1,
+                ),
+                jnp.outer(
+                    expected_weighted_stack_2,
+                    expected_weighted_stack_2,
+                ),
+            ]
         ),
         rtol=1e-5,
     )

e1ffd6bc51880443f0847ea033332dfbc45dcecb
Nowell Closser <nowell91@gmail.com>
2025-10-11 00:15:56 -0400

    Add a new lambda-accpeting estimating function for integration tests

---
+++ functions_to_pass_to_analysis/RL_least_squares_estimating_function_regularized.py
@@ -0,0 +1,39 @@
+import jax
+from jax import numpy as jnp
+
+
+@jax.jit
+def RL_least_squares_loss_regularized(
+    beta_est,
+    base_states,
+    treat_states,
+    actions,
+    rewards,
+    action1probs,
+    action1probtimes,  # pylint: disable=unused-argument
+    action_centering,
+    lambda_,
+    n,
+):
+
+    beta_0_est = beta_est[: base_states.shape[1]].reshape(-1, 1)
+    beta_1_est = beta_est[base_states.shape[1] :].reshape(-1, 1)
+
+    actions = jnp.where(
+        action_centering, actions.astype(jnp.float32) - action1probs, actions
+    )
+    # The -2 is not necessary, but makes this strictly the derivative of the corresponding
+    # loss function as implemented.  This is useful for testing.
+    return (
+        -2
+        * jnp.sum(
+            (
+                rewards
+                - jnp.matmul(base_states, beta_0_est)
+                - jnp.matmul(actions * treat_states, beta_1_est)
+            )
+            * jnp.hstack((base_states, actions * treat_states)),
+            axis=0,
+        )
+        + (2 * lambda_ / n) * beta_est
+    )
